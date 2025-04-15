1751986e5ed1d67df77c954d06b3579f81ef5483
RedEmber-hub <redember38@gmail.com>
2025-04-15 13:25:49 +0300

    добавила выходные курсивом, сегодня жирным, рефакторинг кода

--- lesson07/less07.js
+++ lesson07/less07.js
@@ -1,11 +1,23 @@
-const weekArray = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const renderWeek = () => {
+    const weekArray = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+    const weekends = ['Суббота', 'Воскресенье'];
+    const today = new Date().toLocaleDateString('ru-RU', { weekday: 'long' });
 
-const container = document.createElement('div'); //контейнер <div></div>
+    const container = document.createElement('div'); //контейнер <div></div>
+    document.body.appendChild(container); //добавление контейнера в body
 
-weekArray.forEach(day => {
-    const p = document.createElement('p'); //создание тега <p></p>
-    p.textContent = day; //добавление текста в тег из массива
-    container.appendChild(p); // добавление тега в контейнер <div><p></p></div>
-})
+    weekArray.forEach(day => {
+        const p = document.createElement('p'); //создание тега <p></p>
+        p.textContent = day; //добавление текста в тег из массива
+        container.appendChild(p); // добавление тега в контейнер <div><p></p></div>
 
-document.body.appendChild(container); //добавление контейнера в body
+        if (weekends.includes(day)) {
+            p.style.fontStyle = 'italic';
+        }
+        if (day.toLowerCase() === today.toLowerCase()) {
+            p.style.fontWeight = '700';
+        }
+    })
+}
+
+renderWeek();
