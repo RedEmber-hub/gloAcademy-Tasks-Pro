c44e14b82377f165f3c5df94217ca048a86385dc
RedEmber-hub <redember38@gmail.com>
2025-04-15 12:50:53 +0300

    рефакторинг кода

--- lesson07/less07.js
+++ lesson07/less07.js
@@ -1,5 +1,11 @@
 const weekArray = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
-weekArray.forEach(week => {
-    document.body.innerHTML += `<p>${week}</p>`
+const container = document.createElement('div'); //контейнер <div></div>
+
+weekArray.forEach(day => {
+    const p = document.createElement('p'); //создание тега <p></p>
+    p.textContent = day; //добавление текста в тег из массива
+    container.appendChild(p); // добавление тега в контейнер <div><p></p></div>
 })
+
+document.body.appendChild(container); //добавление контейнера в body
